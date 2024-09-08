0bc95f08cac0278f39b29ff2122cc237376811af
NhatMinh <@dstroke85@gmail.com>
2024-09-08 23:55:53 +0700

    landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import CommonInfoArea from "@/containers/CommonInfoArea";
 import Header from "@/components/Header";
 import Cookies from "js-cookie";
 import { isAuthenticated } from "@/services/auth";
@@ -25,15 +26,31 @@ const Home = () => {
   }
 
   return (
-    <div>
+    <div className="h-screen bg-background">
       <Header userName={user.userName} userRole={user.userRole} />
-      {/* {user.userRole === "admin" ? (
-        <AdminHomePage />
-      ) : user.userRole === "candidate" ? (
-        <CandidateHomePage />
-      ) : (
-        <p className="text-red-500">Error: Unknown role</p>
-      )} */}
+      <div className="flex-grow flex flex-col lg:flex-row">
+        <div className="lg:w-2/5 p-4">
+          <div className="h-full">
+            <CommonInfoArea />
+          </div>
+        </div>
+
+        <div className="lg:w-3/5 flex flex-col h-full">
+          <div className="h-1/3 p-4 bg-accent bg-opacity-20">
+            <div className="h-full border-2 border-dashed border-accent">
+              <h2 className="text-2xl font-bold">Phần phải trên (1/3)</h2>
+            </div>
+          </div>
+
+          {/* Phần dưới bên phải chiếm 2/3 chiều cao còn lại */}
+          <div className="h-2/3 p-4 bg-green-500 bg-opacity-20">
+            <div className="h-full border-2 border-dashed border-green-500">
+              <h2 className="text-2xl font-bold">Phần phải dưới (2/3)</h2>
+              {/* Nội dung của RightBottomComponent */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
