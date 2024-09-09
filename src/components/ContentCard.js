9e8e9a2297dac8fddb359148e5eff382ef41b4ca
dstroke85 <dstroke85@gmail.com>
2024-09-09 12:34:21 +0000

    fixing UI page

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,6 +1,6 @@
 const ContentCard = ({ content, color, title, subTitle, image }) => {
     return (
-        <div className={`flex-1 flex flex-col gap-1 ${color || "bg-red-200"} p-3 rounded-lg`}>
+        <div className={`flex-1 flex flex-col gap-1 ${`bg-${color} || "bg-red-200"`} p-3 rounded-lg`}>
             {title && <h4 className="text-black text-2xl font-semibold">{title}</h4>}
             {content && <p className="text-black text-xs">
                 {content}
