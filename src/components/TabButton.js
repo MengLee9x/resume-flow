2aab0974ad91af203a50deba0a9316f44a888dc8
NhatMinh <@dstroke85@gmail.com>
2024-09-10 13:29:37 +0700

    fix UI

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 const TabButton = ({ activeTab, tabName, onClick }) => {
     const tabNameLowercase = tabName.toLowerCase();
 
     return (
-        <li className={`cursor-pointer text-black text-xs p-5 rounded-2xl ${activeTab === tabNameLowercase ? "bg-gradient-to-r from-orange-400 to-red-500 text-white" : "bg-slate-200"}`} onClick={onClick}>
-            {tabName}
+        <li className={`cursor-pointer text-black text-xs p-5 rounded-2xl w-16 h-16 flex flex-col items-center justify-center ${activeTab === tabNameLowercase ? "bg-gradient-to-r from-orange-400 to-red-500 text-white" : "bg-slate-200"}`} onClick={onClick}>
+            <Image src={`/images/${tabNameLowercase}.png`} alt={tabName} width={20} height={20} />
+            <span>{tabName}</span>
         </li>
     )
 }
